0463a21b452b15cd88f28643b9f9c4d829718e59
anjijava16 <anjiajava16@gmail.com>
2014-12-22 02:06:35 +0800

    Added Add_One Static value

---
+++ RmsSystem/src/com/iwinner/rms/constants/RMSConstants.java
@@ -0,0 +1,9 @@
+package com.iwinner.rms.constants;
+
+import java.util.ResourceBundle;
+
+public class RMSConstants {
+public static ResourceBundle CONFIG=ResourceBundle.getBundle("com.iwinner.irpc.utils.irpc");
+
+public static Integer ADD_ONE=1;
+}
